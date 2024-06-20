234012fd7548b7511a76faafed473d46db8f202a
Tik6263 <62488347+AtsOhata@users.noreply.github.com>
2024-06-20 21:01:42 +0900

    SHA-1 Password Cracker

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,25 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    with open('top-10000-passwords.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            if not use_salts:
+                sha1_hash = hashlib.sha1()
+                sha1_hash.update(line.strip().encode('utf-8'))
+                if hash == sha1_hash.hexdigest():
+                    return line.strip()
+            else:
+                with open('known-salts.txt', 'r', encoding='utf-8') as salt_file:
+                    for salt in salt_file:
+                        sha1_hash = hashlib.sha1()
+                        word = line.strip() + salt.strip()
+                        sha1_hash.update(word.encode('utf-8'))
+                        if hash == sha1_hash.hexdigest():
+                            return line.strip()
+                        sha1_hash = hashlib.sha1()
+                        word = salt.strip() + line.strip()
+                        sha1_hash.update(word.encode('utf-8'))
+                        if hash == sha1_hash.hexdigest():
+                            return line.strip()
+                
+    return "PASSWORD NOT IN DATABASE"
